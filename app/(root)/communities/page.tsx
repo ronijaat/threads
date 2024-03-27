4cff4538891c1dd1191c26f341f726f8636cda0a
ronijaat <raunakbaliyan01@gmail.com>
2024-03-27 12:20:53 +0530

    add search bar

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 
 import CommunityCard from '@/components/cards/CommunityCard';
 // import Pagination from '@/components/shared/Pagination';
-// import Searchbar from '@/components/shared/Searchbar';
+import Searchbar from '@/components/shared/SearchBar';
 
 import { fetchCommunities } from '@/lib/actions/community.actions';
 import { fetchUser } from '@/lib/actions/user.action';
@@ -29,7 +29,9 @@ async function Page({
     <>
       <h1 className="head-text">Communities</h1>
 
-      <div className="mt-5">{/* <Searchbar routeType="communities" /> */}</div>
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
 
       <section className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
